b5fbc452dfb9e977f87d6d85450bca7ee4ce045b
Kurohige <franciscoabayomi69@gmail.com>
2022-10-13 07:59:22 +0100

    four

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * This is my first program using the put command
+ *
+ * Return must be equal to 0
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
