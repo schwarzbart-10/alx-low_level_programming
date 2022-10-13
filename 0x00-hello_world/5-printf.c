17d01ef9c04bcce55b5a88782851507d953be41e
Kurohige <franciscoabayomi69@gmail.com>
2022-10-13 08:27:02 +0100

    five

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - runs all the code
+ *
+ * Description: Using the prinf function to print strings
+ * Main: Displays the value of the prinf function
+ * Return: return 0 to the exit function
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
