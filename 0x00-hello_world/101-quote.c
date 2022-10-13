613c856df6dcee06bd22b0064b29a8b2ecf617d3
Kurohige <franciscoabayomi69@gmail.com>
2022-10-13 09:53:47 +0100

    101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+/**
+ * main - runs the enitre code
+ *
+ * Description: prints the value of the write command
+ * Main: displays the value of the ran code
+ * Return: return 1 to the exit function
+ */
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19,\n", 61);
+	return (1);
+}
