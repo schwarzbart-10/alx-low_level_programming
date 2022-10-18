5069778b18963c734bd7f6d1a9fef07d02186468
Kurohige <franciscoabayomi69@gmail.com>
2022-10-18 12:52:03 +0100

    zero

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+* main - the entry point for the program
+* Description: The code prints _putchar
+* Return: returns 0 to the exit function
+*/
+int main(void)
+{
+	printf("_putchar\n");
+
+	return (0);
+}
