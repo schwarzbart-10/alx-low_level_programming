b6d898a9e5688e883a79bada76b6ae0bbbd84bf4
Kurohige <franciscoabayomi69@gmail.com>
2022-10-18 13:44:34 +0100

    four

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for lowercase character
+ * @c: the character to check
+ * Return: 1 if c is lowercase, 0 otherwise
+ */
+int _isalpha(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
